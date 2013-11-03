a77a4dcb679ed6fb769a013e6c3903b004fa478b
Michał Świętochowski <m.swietochowski@insee.pl>
2013-11-03 17:36:12 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+PROI-LAB1
+=========
+
+Laboratorium PROI 1, semestr 13Z, EiTI. Bufor Cykliczny. Prowadzący mgr inż. Mariusz Zubrzycki
+
---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+/**
+ * main file
+ */
+
+ #include <iostream>
+
+ using namespace std;
+
+ int main() {
+ 	cout << "Hello world" << endl;
+ 	return 0;
+ }
